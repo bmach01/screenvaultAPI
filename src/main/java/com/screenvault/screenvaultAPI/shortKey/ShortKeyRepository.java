3a0ecf73b94bed76deff9151df572c47dd4b9157
Bartosz Machniak <b.machniak01@gmail.com>
2024-11-30 23:36:38 +0100

    Delete shortKey

--- src/main/java/com/screenvault/screenvaultAPI/shortKey/ShortKeyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.screenvault.screenvaultAPI.shortKey;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface ShortKeyRepository extends MongoRepository<ShortKey, String> {
-    boolean existsByKey(String Key);
-}
